745df031ca77b166dc34abacf8baca2234d1d4a5
jamesspencer19 <73702889+jamesspencer19@users.noreply.github.com>
2021-10-14 10:26:51 +0100

    Basic Menu Implemented

--- src/main/java/com/adaship/AdaShip.java
+++ src/main/java/com/adaship/AdaShip.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 public class AdaShip {
     public static void main(String[] args) throws IOException {
-        configReader.readConfigJSON();
-        char[][] gameboard = createBoard.createGameBoard(configReader.getBoardLength(),configReader.getBoardWidth(),configReader.getWater());
-        createBoard.printGameBoard(gameboard, configReader.getWater(), configReader.getShip(),configReader.getHit(),configReader.getMiss());
+        AdaShipMenu.menu();
     }
 }
---
+++ src/main/java/com/adaship/AdaShipMenu.java
@@ -0,0 +1,36 @@
+package com.adaship;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AdaShipMenu {
+    public static void menu() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("⛵-------------- AdaShip --------------⛵");
+        System.out.println("              |    |    |                 \n" +
+                "             )_)  )_)  )_)              \n" +
+                "            )___))___))___)\\            \n" +
+                "           )____)____)_____)\\\\\n" +
+                "         _____|____|____|____\\\\\\__\n" +
+                "---------\\                   /---------\n" +
+                "  ^^^^^ ^^^^^^^^^^^^^^^^^^^^^\n" +
+                "    ^^^^      ^^^^     ^^^    ^^\n" +
+                "         ^^^^      ^^^");
+        System.out.println("Menu: \n" + "1. One player v computer game\n" + "2. Quit\n");
+        int answer = sc.nextInt();
+        switch (answer){
+            case 1:{
+                configReader.readConfigJSON();
+                char[][] gameboard = createBoard.createGameBoard(configReader.getBoardLength(),configReader.getBoardWidth(),configReader.getWater());
+                createBoard.printGameBoard(gameboard, configReader.getWater(), configReader.getShip(),configReader.getHit(),configReader.getMiss());
+            }
+            break;
+            case 2: {
+                for (int i = 0; i < 25; i++) {
+                    System.out.println("\n");
+                }
+            }
+            break;
+        }
+    }
+}
